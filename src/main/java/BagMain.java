798767dde58bcd26d8940d3d7216656fed687bba
nishi003 <96027834+nishi003@users.noreply.github.com>
2022-10-09 16:14:12 -0400

    attempt 1

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -15,7 +15,22 @@ class BagMain {
      * on new Bag types (and HandBag subclasses)!
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
-        // TODO: Implement this.
+        if (double_enhance_handbags == true) {
+            for (Bag bag : bags) {
+                if (bag instanceof HandBag) {
+                    bag.enhance();
+                    bag.enhance();
+                }
+                else {
+                    bag.enhance();
+                }
+            }
+        }
+        else{
+            for (Bag bag : bags){
+                bag.enhance();
+            }
+        }
     }
 
     /**
@@ -28,6 +43,12 @@ class BagMain {
      * @return The total number of straps of CrossbodyBags.
      */
     public static int countCrossbodyStraps(Bag[] bags) {
-        // TODO: Implement this.
+        int countStraps = 0;
+        for (Bag bag : bags){
+            if (bag instanceof CrossbodyBag){
+                countStraps = countStraps + ((CrossbodyBag)bag).getNumberOfStraps();
+            }
+        }
+        return countStraps;
     }
 }
